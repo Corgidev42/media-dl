394c3a622608760a9173723e2d3ccf818b1a9980
vbonnard <vbonnard.dev@gmail.com>
2025-04-18 17:22:28 +0200

    setup.py upd

--- setup.py
+++ setup.py
@@ -1,12 +1,26 @@
 from setuptools import setup
 
 setup(
-    name='media-dl',
-    version='1.0',
-    py_modules=['media_dl'],
-    install_requires=['yt-dlp', 'mutagen', 'requests'],
-    entry_points='''
-        [console_scripts]
-        media-dl=media_dl:main
-    ''',
+	name="media-dl",
+	version="1.0.0",
+	description="Universal Media Downloader for YouTube, SoundCloud and more.",
+	author="Vincent",
+	author_email="vbonnard.dev@gmail.com",
+	py_modules=["media_dl"],
+	install_requires=[
+		"yt-dlp",
+		"mutagen",
+		"requests",
+	],
+	entry_points={
+		'console_scripts': [
+			'media-dl = media_dl:main',
+		],
+	},
+	classifiers=[
+		"Programming Language :: Python :: 3",
+		"License :: OSI Approved :: MIT License",
+		"Operating System :: OS Independent",
+	],
+	python_requires='>=3.7',
 )
